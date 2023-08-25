27eebe9f3fa3b6bc34fd8da6c5b8f2bc0922670f
jamesmhee <nontasabs@gmail.com>
2023-08-26 04:05:22 +0700

    v4

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,34 +1,62 @@
 import React, { useEffect, useState } from "react";
 import { getAssets } from "../functions/Getdata";
 import { Link } from "react-router-dom";
-import { MdAnalytics } from "react-icons/md";
-import Search from "./Search";
+import { MdAnalytics, MdSearch } from "react-icons/md";
 import Switch from "./Switch";
 
 const Table = () => {
   const [assets, setAssets] = useState([]);
+  const [filteredText, setFilteredText] = useState([]);
+  const [searchText, setSearchText] = useState([]);
   const [loading, setLoading] = useState(false);
 
   const fetchAsset = async () => {
     try {
       const response = await getAssets();
       setAssets(response?.data);
+      setLoading(true);
     } catch (error) {
       console.log("error", error);
     }
+    setLoading(true)
   };
+
   useEffect(() => {
-    const fetchData = async () => {
-      await fetchAsset();
-      setLoading(true);
-    };
-    fetchData();
-  }, [assets]);
+    fetchAsset();
+  }, []);
+
+  const filterAsset = (searchText) => {
+    if (searchText.trim().length > 0) {
+      const filter = assets.filter((asset) => {
+        return (
+          asset?.name.toLowerCase().includes(searchText.toLowerCase()) ||
+          asset?.symbol.toLowerCase().includes(searchText.toLowerCase())
+        );
+      });
+      setFilteredText(filter);
+    } else {
+      setFilteredText(assets);
+    }
+  };
 
   return (
     <>
       <div className="flex justify-between mb-3">
-        <Search />
+        <div className="inline-flex border p-1 bg-light-bg dark:bg-dark-bg w-auto dark:border-[1.5px] rounded-lg mt-2 relative">
+          <div className="my-auto pointer-events-auto">
+            <MdSearch className="border-r-2 dark:text-light-bg" size={25} />
+          </div>
+          <input
+            className="text-xs md:text-base outline-0 px-1 bg-transparent dark:text-light-elem"
+            type="text"
+            placeholder="Search"
+            onChange={(e) => {
+              setSearchText(e.target.value);
+              filterAsset(e.target.value);
+            }}
+            value={searchText}
+          />
+        </div>
         <Switch />
       </div>
       <div className="mx-auto my-2 overflow-x-auto md:overflow-hidden rounded-lg">
@@ -53,51 +81,56 @@ const Table = () => {
           </thead>
           {loading ? (
             <>
-              {assets.map((asset, index) => (
-                <tbody
-                  key={asset.id}
-                  className="antialiased text-dark-elem dark:text-light-elem font-medium"
-                >
-                  <tr className="hover:bg-zinc-100 dark:hover:bg-slate-600 h-20 border-b dark:border-b">
-                    <td className="pl-5 text-sm">{index + 1}</td>
-                    <td>
-                      <Link to={`/asset/` + asset.id}>
-                        {asset?.name}
-                        <br></br>
-                        <label className="cursor-pointer text-sm md:hidden">
-                          <MdAnalytics className="inline-block fill-yellow-500 dark:fill-light-red" />{" "}
-                          :{" "}
-                          {parseFloat(asset?.marketCapUsd).toLocaleString(
-                            "en-US",
-                            {
-                              maximumFractionDigits: 0,
-                            }
-                          )}
-                        </label>
-                      </Link>
-                    </td>
-                    <td className="text-center">
-                      {parseFloat(asset?.priceUsd).toLocaleString("en-US", {
-                        minimumFractionDigits: 2,
-                        maximumFractionDigits: 2,
-                      })}
-                    </td>
-                    <td className="text-right hidden md:table-cell">
-                      {parseFloat(asset?.marketCapUsd).toLocaleString("en-US", {
-                        maximumFractionDigits: 0,
-                      })}
-                    </td>
-                    <td className="pr-5 text-right">
-                      {parseFloat(asset?.volumeUsd24Hr).toLocaleString(
-                        "en-US",
-                        {
-                          maximumFractionDigits: 0,
-                        }
-                      )}
-                    </td>
-                  </tr>
-                </tbody>
-              ))}
+              {(filteredText.length > 0 ? filteredText : assets).map(
+                (asset, index) => (
+                  <tbody
+                    key={asset.id}
+                    className="antialiased text-dark-elem dark:text-light-elem font-medium"
+                  >
+                    <tr className="hover:bg-zinc-100 dark:hover:bg-slate-600 h-20 border-b dark:border-b">
+                      <td className="pl-5 text-sm">{index + 1}</td>
+                      <td>
+                        <Link to={`/asset/` + asset.id}>
+                          {asset?.name}
+                          <br></br>
+                          <label className="cursor-pointer text-sm md:hidden">
+                            <MdAnalytics className="inline-block fill-yellow-500 dark:fill-light-red" />{" "}
+                            :{" "}
+                            {parseFloat(asset?.marketCapUsd).toLocaleString(
+                              "en-US",
+                              {
+                                maximumFractionDigits: 0,
+                              }
+                            )}
+                          </label>
+                        </Link>
+                      </td>
+                      <td className="text-center">
+                        {parseFloat(asset?.priceUsd).toLocaleString("en-US", {
+                          minimumFractionDigits: 2,
+                          maximumFractionDigits: 2,
+                        })}
+                      </td>
+                      <td className="text-right hidden md:table-cell">
+                        {parseFloat(asset?.marketCapUsd).toLocaleString(
+                          "en-US",
+                          {
+                            maximumFractionDigits: 0,
+                          }
+                        )}
+                      </td>
+                      <td className="pr-5 text-right">
+                        {parseFloat(asset?.volumeUsd24Hr).toLocaleString(
+                          "en-US",
+                          {
+                            maximumFractionDigits: 0,
+                          }
+                        )}
+                      </td>
+                    </tr>
+                  </tbody>
+                )
+              )}
             </>
           ) : (
             <tbody className="text-dark-elem dark:text-light-elem">
